5d3dfa2e99fcbe3bc87669df9c7e64bd7e495a6a
beauspot <42305168+beauspot@users.noreply.github.com>
2026-04-14 19:18:18 +0100

    (feat): setup routes for registering, logging in & logging out users

---
+++ src/api/routes/user.routes.ts
@@ -0,0 +1,46 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { container } from "tsyringe";
+
+import UserController from "@/controllers/userCtrl";
+import { validate } from "@/helpers/middlewares/validate";
+import { createUserSchema, loginUserSchema } from "@/schemas/user.schema";
+import UserService from "@/services/user.service";
+
+const userRouter = () => {
+  const router = Router();
+
+  const userService = container.resolve(UserService);
+  const userController = new UserController(userService);
+
+  router.post(
+    "/register",
+    validate(createUserSchema),
+    (req: Request, res: Response, next: NextFunction) => {
+      return userController.registerUser(req, res, next);
+    },
+  );
+
+  router.post(
+    "/login",
+    validate(loginUserSchema),
+    (req: Request, res: Response, next: NextFunction) => {
+      return userController.LoginUser(req, res, next);
+    },
+  );
+
+  router.post("/logout", (req: Request, res: Response, next: NextFunction) => {
+    return userController.LogoutUser(req, res, next);
+  });
+
+  router.post("/forgotPassword", (req: Request, res: Response) => {
+    return userController.fgt_pwd(req, res);
+  });
+
+  router.post("/resetpassword", (req: Request, res: Response) => {
+    return userController.reset_pwd(req, res);
+  });
+
+  return router;
+};
+
+export default userRouter();
