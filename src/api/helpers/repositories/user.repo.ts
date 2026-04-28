55b84a83cf0e6f6ce806c64a4080425b881055bb
beauspot <42305168+beauspot@users.noreply.github.com>
2026-04-28 08:07:42 +0100

    (feat): add a findUserWithAccount method and also comment out the findByPasswordResetToken method because it is no longer needed since redis now stores all the Tokens and OTPs rather than the Database

--- src/api/helpers/repositories/user.repo.ts
+++ src/api/helpers/repositories/user.repo.ts
@@ -29,4 +29,46 @@ export class UserRepository {
       where: { id: userId } as FindOptionsWhere<User>,
     });
   }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.repository.findOne({ where: { email } });
+  }
+
+  /*   async findByPasswordResetToken(token: string): Promise<User | null> {
+    return this.repository.findOne({
+      where: { passwordResetToken: token },
+    });
+  } */
+
+  async findUserWithAccount(userId: string): Promise<User | null> {
+    return this.repository.findOne({
+      where: { id: userId },
+      relations: ["account"],
+      select: {
+        id: true,
+        firstname: true,
+        lastname: true,
+        email: true,
+        phonenumber: true,
+        isEmailVerified: true,
+        accountStatus: true,
+        profilePhoto: true,
+        createdAt: true,
+        account: {
+          id: true,
+          accountNumber: true,
+          accountName: true,
+          balance: true,
+          ledgerBalance: true,
+          paystackBankName: true,
+          type: true,
+          status: true,
+        },
+      },
+    });
+  }
+
+  async updateProfilePhoto(userId: string, photoPath: string): Promise<void> {
+    await this.repository.update(userId, { profilePhoto: photoPath });
+  }
 }
