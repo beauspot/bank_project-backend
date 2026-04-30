d90dd865c52c4716b6ea524c53cb954989d3fb66
beauspot <42305168+beauspot@users.noreply.github.com>
2026-04-30 07:42:35 +0100

    (chore): add the userRole Property to the sessions, aiding to help identify the type of the users logged in on the server

--- src/api/helpers/types/sessions.d.ts
+++ src/api/helpers/types/sessions.d.ts
@@ -1,9 +1,11 @@
 import "express-session";
 
+import { UserRole } from "@/enums/user";
 declare module "express-session" {
   interface SessionData {
     userId: string;
     isLoggedIn: boolean;
     createdAt: Date;
+    userRole: UserRole; // storing the user role in the session
   }
 }
