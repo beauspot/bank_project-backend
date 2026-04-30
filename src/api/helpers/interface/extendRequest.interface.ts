6124d556d1bbd9a7769e9be84e2da6186112fd75
beauspot <42305168+beauspot@users.noreply.github.com>
2026-04-30 08:08:41 +0100

    (chore): add the role Property setting it to the userRole enum & also took out the undefined from the sub properties of user, & also add the role property to the ExtendResponse interface

--- src/api/helpers/interface/extendRequest.interface.ts
+++ src/api/helpers/interface/extendRequest.interface.ts
@@ -1,20 +1,23 @@
 import { Request, Response } from "express";
 
+import { UserRole } from "@/enums/user";
 import { User } from "@/models/userEntity";
 
 export interface ExtendRequest extends Request {
   body: any;
   user?: {
-    id?: string;
-    email?: string;
-    firstname?: string;
-    lastname?: string;
-    phonenumber?: string;
+    id: string;
+    email: string;
+    firstname: string;
+    lastname: string;
+    phonenumber: string;
+    role: UserRole;
   };
 }
 
 export interface ExtendResponse extends Response {
   locals: {
     user?: User;
+    role: UserRole;
   };
 }
